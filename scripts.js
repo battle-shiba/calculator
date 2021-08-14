c744f07217680fa44a93ed9c0e6824d4b370533b
Brandon Fasy <bfasy@uci.edu>
2021-08-13 17:29:40 -0700

    working! needs to be styled better but good prototype

--- scripts.js
+++ scripts.js
@@ -2,8 +2,25 @@ const display = document.querySelector('.display');
 
 const btns = document.querySelectorAll('.button');
 
+const operatorBtns = document.querySelectorAll('.operator')
+
+const addBtn = document.querySelector('#add');
+
+const mulBtn = document.querySelector('#multiply');
+
+const subBtn = document.querySelector('#subtract');
+
+const divBtn = document.querySelector('#divide');
+
+const clearBtn = document.querySelector('#clr');
+
+const equalsBtn = document.querySelector('#equals');
+
 let digitBtns = [];
 
+let operator = 0;
+let ans = +display.textContent;
+//----------------------------------------------------------------//
 btns.forEach((btn) => {
     if ( !isNaN(btn.textContent) ) {
         digitBtns.push(btn);
@@ -14,15 +31,50 @@ btns.forEach((btn) => {
 
 digitBtns.forEach((btn) => {
     btn.addEventListener('click', function() {
+        
         display.textContent = btn.textContent;
+        
+
+        if (operator) {
+            ans = operate(operator,ans,btn.textContent)
+        }
+
+        else {
+            ans = +display.textContent;
+        }
 
     });
 });
 
+addBtn.addEventListener('click', function() {
+    operator = add;
+});
+
+mulBtn.addEventListener('click', function() {
+    operator = multiply;
+});
+
+subBtn.addEventListener('click', function() {
+    operator = subtract;
+});
+
+divBtn.addEventListener('click', function() {
+    operator = divide;
+});
+
+clearBtn.addEventListener('click', clear);
+
+equalsBtn.addEventListener('click', function() {
+    display.textContent = ans;
+})
 
 
+
+//----------------------------------------------------------------//
 
 function add(m,n) {
+    m= +m;
+    n = +n;
     return m+n;
 }
 
@@ -31,6 +83,8 @@ function subtract(m,n) {
 }
 
 function multiply(m,n) {
+    m = +m;
+    n = + n;
     return m*n;
 }
 
@@ -42,3 +96,11 @@ function operate(operator,m,n) {
     return operator(m,n);
 }
 
+
+//----------------------------------------------------------------//
+
+function clear() {
+    operator = 0;
+    ans = 0;
+    display.textContent = 0;
+}
